dbec136554018aff889f734ec1d8a71cbbcc0f7f
pankaj-deepnapsoftech <itsybizz@gmail.com>
2026-03-25 12:52:15 +0530

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,15 +5,11 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
